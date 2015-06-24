011bc2c7ae0ac918410cb1954a69c0999fd8a8d5
assam1231 <assam1231@gmail.com>
2015-06-24 12:22:58 +0800

    Allow multiple seam
    Some Bugs still

--- DeleteSeam.m
+++ DeleteSeam.m
@@ -1,10 +1,15 @@
 function DeleteSeam(vector, I)
 
 	[rmax, cmax, tmp] = size(I);
-	for row=1:rmax
-		col = vector(row);
-		I(row, col:cmax-1,:) = I(row, col+1:cmax,:);
-		
+	[rmax, num] = size(vector);
+	for h=1:num
+		for row=1:rmax
+			[col, index] = max(vector(row, :));
+			I(row, col:cmax-1,:) = I(row, col+1:cmax,:);
+			vector(row, index) = 0;
+		end
+		I(:, cmax, :) = [];
+		cmax = cmax-1;
 	end
-	I(:, cmax, :) = [];
+	
 imwrite(I, 'c.png');
