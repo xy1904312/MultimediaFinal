f84f3edd3a9885b60d6b32052f02fde310263c86
assam1231 <assam1231@gmail.com>
2015-06-25 17:15:50 +0800

    User indentify object

---
+++ identifyObject.m
@@ -0,0 +1,30 @@
+function Gimg = identifyObject(img)
+	%initial energy
+	Gimg = getEnergy(img);
+	
+	%get user input
+	imshow(img);
+	[PointX, PointY] = ginput(200);
+	PointList = [PointX PointY];
+	
+	%create points inside polygon
+	[row, col, ~] = size(img);
+	
+	[imgX,imgY] = meshgrid(1:row,1:col);
+	imgXY = imgX;
+	imgXY(:,:,2) = imgY;
+	imgPixN = row*col;
+	imgXYList = reshape(imgXY, [ imgPixN, 2]);
+	
+	isInShape = inpolygon( imgXYList(:,1), imgXYList(:,2), PointList(:,1), PointList(:,2) );
+	
+	
+	for pixI = 1 : imgPixN
+		if isInShape(pixI)
+			xCoord = imgXYList(pixI,1);
+			yCoord = imgXYList(pixI,2);
+			%set energy here
+			Gimg(xCoord, yCoord) = realmax;
+		end
+	end
+end
